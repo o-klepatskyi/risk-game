bb027f23985ff465cdb6967aeb6cca48111406e5
navimakarov <makarov18042003@gmail.com>
2021-05-21 22:53:33 +0300

    Added menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        Menu menu = new Menu(frame);
+        menu.setFocusable(true);
+        frame.add(menu);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,186 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Menu extends JPanel {
+    private final JButton play, multiplayer, rules;
+    private int menuOptionChosen;
+    public Menu(JFrame frame) {
+        final int SIZE = 500;
+
+        this.setPreferredSize(new Dimension(SIZE, SIZE));
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(85, 0, -70, 0);
+
+        play = new JButton("  Play  ");
+        play.setFont(new Font("Arial", Font.PLAIN, 30));
+        play.setContentAreaFilled(false);
+        play.setMargin(new Insets(0, 50, 0, 50));
+        play.setBorderPainted(false);
+        play.setFocusPainted(false);
+        play.setForeground(Color.WHITE);
+        play.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                // TODO
+            }
+
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                menuOptionChosen = 1;
+                highlightOption(menuOptionChosen);
+            }
+
+            public void mouseExited(MouseEvent e) {
+                play.setForeground(Color.WHITE);
+                resetButtons();
+            }
+        });
+
+        multiplayer = new JButton("Multiplayer");
+        multiplayer.setFont(new Font("Arial", Font.PLAIN, 30));
+        multiplayer.setContentAreaFilled(false);
+        multiplayer.setBorderPainted(false);
+        multiplayer.setFocusPainted(false);
+        multiplayer.setForeground(Color.WHITE);
+        multiplayer.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                // TODO
+            }
+
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                menuOptionChosen = 2;
+                highlightOption(menuOptionChosen);
+            }
+
+            public void mouseExited(MouseEvent e) {
+                resetButtons();
+            }
+        });
+
+        rules = new JButton("Rules");
+        rules.setFont(new Font("Arial", Font.PLAIN, 30));
+        rules.setContentAreaFilled(false);
+        rules.setBorderPainted(false);
+        rules.setFocusPainted(false);
+        rules.setForeground(Color.WHITE);
+        rules.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                // TODO
+            }
+
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            public void mouseEntered(MouseEvent e) {
+                menuOptionChosen = 3;
+                highlightOption(menuOptionChosen);
+            }
+
+            public void mouseExited(MouseEvent e) {
+                resetButtons();
+            }
+        });
+
+        add(play, gbc);
+        add(multiplayer, gbc);
+        add(rules, gbc);
+
+        menuOptionChosen = 1;
+        highlightOption(menuOptionChosen);
+
+        this.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if(key == KeyEvent.VK_UP) {
+                    if(menuOptionChosen == 1)
+                        menuOptionChosen = 3;
+                    else
+                        menuOptionChosen--;
+                    highlightOption(menuOptionChosen);
+                }
+                else if(key == KeyEvent.VK_DOWN) {
+                    if(menuOptionChosen == 3)
+                        menuOptionChosen = 1;
+                    else
+                        menuOptionChosen++;
+                    highlightOption(menuOptionChosen);
+                }
+                else if(key == KeyEvent.VK_ENTER) {
+                    // TODO
+                }
+
+            }
+
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ImageIcon backgroundImage = new ImageIcon("res/menu_bg.jpg");
+        g.drawImage(backgroundImage.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+        ImageIcon menuPanel = new ImageIcon("res/woodSign.jpg");
+        g.drawImage(menuPanel.getImage(), 122, 225, 250, 200, null);
+    }
+
+    private void resetButtons() {
+        play.setForeground(Color.WHITE);
+        play.setText("  Play  ");
+
+        multiplayer.setForeground(Color.WHITE);
+        multiplayer.setText("  Multiplayer  ");
+
+        rules.setForeground(Color.WHITE);
+        rules.setText("  Rules  ");
+    }
+
+    private void highlightOption(int option) {
+        resetButtons();
+        if(option == 1) {
+            play.setForeground(Color.YELLOW);
+            play.setText("< Play >");
+        }
+        else if(option == 2) {
+            multiplayer.setForeground(Color.YELLOW);
+            multiplayer.setText("< Multiplayer >");
+        }
+        else if(option == 3) {
+            rules.setForeground(Color.YELLOW);
+            rules.setText("< Rules >");
+        }
+    }
+}

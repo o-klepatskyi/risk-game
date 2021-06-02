a5d9c3d41a91be1be5e11c2219330f2e9ddab735
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2021-06-02 19:54:27 +0300

    Add logo to main menu

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        openGameMenu();
+        openMainMenu();
     }
 
     private static void openMainMenu() {

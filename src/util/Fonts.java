221a97cf72d01ff02e9a80d44622e244d46b7794
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2021-06-02 23:26:25 +0300

    Font fix

--- src/util/Fonts.java
+++ src/util/Fonts.java
@@ -1,9 +1,27 @@
 package util;
 
-import java.awt.Font;
-// todo: do something with fonts (check if installed??)
-public interface Fonts {
-    Font LABEL_FONT  = new Font("Blackadder ITC", Font.BOLD, 25);
-    Font BUTTON_FONT  = new Font("Footlight MT Light", Font.PLAIN, 25);
-    Font FIELD_FONT = new Font("Serif", Font.BOLD, 20);
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public abstract class Fonts {
+    public static final float DEFAULT_SIZE = 25f;
+
+    public static Font LABEL_FONT; // = new Font("Blackadder ITC", Font.BOLD, 25);
+    public static Font BUTTON_FONT; //  = new Font("Footlight MT Light", Font.PLAIN, 25);
+    public static Font FIELD_FONT = new Font("Serif", Font.BOLD, 20);
+
+    static {
+        try {
+            // create fonts
+            LABEL_FONT = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/ITCBLKAD.TTF")).deriveFont(DEFAULT_SIZE);
+            BUTTON_FONT = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/FTLTLT.TTF")).deriveFont(DEFAULT_SIZE);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            //register the font
+            ge.registerFont(LABEL_FONT);
+            ge.registerFont(BUTTON_FONT);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
 }

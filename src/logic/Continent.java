19bbd624888ae655b4762657ce69ff15bbfea5a0
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2021-06-08 13:19:59 +0300

    Create class Map which will contain game graph, continents and image

---
+++ src/logic/Continent.java
@@ -0,0 +1,22 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Continent {
+    public final String name;
+    public final int bonus;
+    private final ArrayList<Territory> territories = new ArrayList<>();
+
+    public Continent(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public void addTerritory(Territory t) {
+        territories.add(t);
+    }
+
+    public ArrayList<Territory> getTerritories() {
+        return territories;
+    }
+}
